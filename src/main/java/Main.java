2c9203101de348811f4141106bcc4aa97f4fb59b
trweiss17 <142759304+trweiss17@users.noreply.github.com>
2024-01-25 12:20:02 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,42 @@ public class Main {
 		PigLatin sim = new PigLatin();
         	//sim.tester();
 	}
-	
+	public void setup() 
+{
+  String[] lines = {"beast","dough","happy","question","star","three","eagle","try"};
+  System.out.println("there are " + lines.length + " lines");
+  for (int i = 0 ; i < lines.length; i++) 
+  {
+    System.out.println(pigLatin(lines[i]));
+  }
+}
+public void draw()
+{
+        //not used
+}
+public int findFirstVowel(String word){
+  int fail= -1;
+    for(int i = 0; i < word.length(); i++)
+       if(word.charAt(i) == 'a' || word.charAt(i) == 'e' || 
+          word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u')
+      return i;
+      return fail;
+}
+
+
+public String pigLatin(String word)
+//precondition: sWord is a valid String of length greater than 0
+//postcondition: returns the pig latin equivalent of sWord
+{
+  if(findFirstVowel(word) == -1)
+  {
+    return word + "ay";
+  }
+  else
+  {
+    return "ERROR!";
+  }
+}
 
 
 }
